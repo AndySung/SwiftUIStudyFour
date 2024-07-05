d2e911d8c50812265d91dbe180888d37b17e3639
andysung <songzongjian0123@163.com>
2024-07-05 15:01:12 +0800

    first commit

---
+++ SwiftUIStudyFour/SwiftUIStudyFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyFourApp.swift
+//  SwiftUIStudyFour
+//
+//  Created by andy on 2024/7/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
